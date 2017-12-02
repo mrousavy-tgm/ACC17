e132f4943f3e660ca38ef3b318dfdb1c957fac96
mrousavy <mrousavy@donotreply.github.com>
2017-12-02 13:36:52 +0100

    Remove unnecessary build script

--- build.sh
+++
@@ -1,8 +0,0 @@
-if ! type "$dotnet" > /dev/null; then		# Only works on sh/bash/zsh
-  echo "dotnet command is not installed/in PATH"
-fi
-
-dotnet restore												# Restore NuGet Packages
-dotnet build ACC17/ACC17.csproj -c Release					# Build Source Project
-dotnet build ACC17_Tests/ACC17_Tests.csproj -c Release		# Build Test Project
-dotnet test ACC17_Tests/ACC17_Tests.csproj -c Release		# Run unit tests
