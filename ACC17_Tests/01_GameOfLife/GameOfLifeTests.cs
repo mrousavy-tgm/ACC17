1022ab4d2c702a4c6a25459b7d403fd6e455a5e0
mrousavy <mrousavy@donotreply.github.com>
2017-12-02 12:19:17 +0100

    Implement Unit Tests for Game of life

---
+++ ACC17_Tests/01_GameOfLife/GameOfLifeTests.cs
@@ -0,0 +1,13 @@
+﻿using ACC17._01_GameOfLife;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ACC17_Tests._01_GameOfLife {
+    [TestClass]
+    public class GameOfLifeTests {
+        [TestMethod]
+        public void TestMethod1() {
+            var classUnderTest = new GameOfLife("2;2;0;1000$");
+            Assert.Equals("2;2;0;1000$\n", classUnderTest.GetPattern(0));
+        }
+    }
+}
